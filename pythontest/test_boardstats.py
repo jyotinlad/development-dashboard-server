775397896f54a96b2938a775b4c465c3b52cdfb4
Jyotin Lad <j.lad.uk@gmail.com>
2020-12-27 12:28:51 +0000

    Adding Last Unit Tests

--- pythontest/test_boardstats.py
+++ pythontest/test_boardstats.py
@@ -1,26 +1,156 @@
+from datetime import date, datetime
+from freezegun import freeze_time
 from unittest import main, TestCase
+from unittest.mock import patch
+
+from python.boardstats import BoardStats
 
 
 class BoardStatsTests(TestCase):
 
+    _BASE_URL = "python.boardstats"
+
     def test_get_end_of_month(self):
-        # return dt.replace(day=monthrange(dt.year, dt.month)[1])
-        pass
+        self.assertEqual(
+            BoardStats._get_end_of_month(datetime(2020, 12, 1)),
+            datetime(2020, 12, 31)
+        )
 
     def test_get_quarter_from_month(self):
-        # return (month - 1) // 3 + 1
-        pass
+        # test quarter 1
+        self.assertEqual(BoardStats._get_quarter_from_month(1), 1)
+        self.assertEqual(BoardStats._get_quarter_from_month(3), 1)
+
+        # test quarter 2
+        self.assertEqual(BoardStats._get_quarter_from_month(4), 2)
+        self.assertEqual(BoardStats._get_quarter_from_month(6), 2)
+
+        # test quarter 3
+        self.assertEqual(BoardStats._get_quarter_from_month(7), 3)
+        self.assertEqual(BoardStats._get_quarter_from_month(9), 3)
+
+        # test quarter 4
+        self.assertEqual(BoardStats._get_quarter_from_month(10), 4)
+        self.assertEqual(BoardStats._get_quarter_from_month(12), 4)
 
     def test_get_quarter_key(self):
-        # quarter = cls._get_quarter_from_month(dt.month)
-        # return cls._get_end_of_month(dt.replace(month=quarter*3, day=1))
-        pass
+        # test quarter 1
+        self.assertEqual(
+            BoardStats._get_quarter_key(datetime(2020, 1, 1)),
+            datetime(2020, 3, 31)
+        )
+        self.assertEqual(
+            BoardStats._get_quarter_key(datetime(2020, 2, 28)),
+            datetime(2020, 3, 31)
+        )
 
-    def test_quarterly(self):
-        pass
+        # test quarter 2
+        self.assertEqual(
+            BoardStats._get_quarter_key(datetime(2020, 4, 1)),
+            datetime(2020, 6, 30)
+        )
+        self.assertEqual(
+            BoardStats._get_quarter_key(datetime(2020, 6, 29)),
+            datetime(2020, 6, 30)
+        )
 
-    def test_monthly(self):
-        pass
+        # test quarter 3
+        self.assertEqual(
+            BoardStats._get_quarter_key(datetime(2020, 7, 1)),
+            datetime(2020, 9, 30)
+        )
+        self.assertEqual(
+            BoardStats._get_quarter_key(datetime(2020, 9, 29)),
+            datetime(2020, 9, 30)
+        )
+
+        # test quarter 4
+        self.assertEqual(
+            BoardStats._get_quarter_key(datetime(2020, 10, 1)),
+            datetime(2020, 12, 31)
+        )
+        self.assertEqual(
+            BoardStats._get_quarter_key(datetime(2020, 12, 31)),
+            datetime(2020, 12, 31)
+        )
+
+    @freeze_time("2020-12-31")
+    @patch(f"{_BASE_URL}.Board")
+    def test_quarterly(self, mock_board):
+        mock_board.return_value.name = "BOARD"
+        mock_board.return_value.completed.return_value = [
+            {"completed_date": datetime(2020, 1, 1, 0, 0)},
+            {"completed_date": datetime(2020, 2, 1, 0, 0)},
+            {"completed_date": datetime(2020, 3, 31, 0, 0)},
+            {"completed_date": datetime(2020, 4, 1, 0, 0)},
+            {"completed_date": datetime(2020, 4, 1, 0, 0)},
+            {"completed_date": datetime(2020, 5, 1, 0, 0)},
+            {"completed_date": datetime(2020, 6, 30, 0, 0)},
+            {"completed_date": datetime(2020, 7, 1, 0, 0)},
+            {"completed_date": datetime(2020, 7, 1, 0, 0)},
+            {"completed_date": datetime(2020, 7, 1, 0, 0)},
+            {"completed_date": datetime(2020, 8, 1, 0, 0)},
+            {"completed_date": datetime(2020, 9, 30, 0, 0)},
+            {"completed_date": datetime(2020, 10, 1, 0, 0)},
+            {"completed_date": datetime(2020, 10, 1, 0, 0)},
+            {"completed_date": datetime(2020, 10, 1, 0, 0)},
+            {"completed_date": datetime(2020, 10, 1, 0, 0)},
+            {"completed_date": datetime(2020, 11, 1, 0, 0)},
+            {"completed_date": datetime(2020, 12, 31, 0, 0)}
+        ]
+
+        self.assertDictEqual(
+            BoardStats.quarterly(1),
+            {
+                "name": "BOARD",
+                "records": {
+                    date(2020, 3, 31): 3,
+                    date(2020, 6, 30): 4,
+                    date(2020, 9, 30): 5,
+                    date(2020, 12, 31): 6,
+                }
+            }
+        )
+
+    @freeze_time("2020-12-31")
+    @patch(f"{_BASE_URL}.Board")
+    def test_monthly(self, mock_board):
+        mock_board.return_value.name = "BOARD"
+        mock_board.return_value.completed.return_value = [
+            {"completed_date": datetime(2020, 1, 1, 0, 0)},
+            {"completed_date": datetime(2020, 2, 1, 0, 0)},
+            {"completed_date": datetime(2020, 3, 31, 0, 0)},
+            {"completed_date": datetime(2020, 4, 1, 0, 0)},
+            {"completed_date": datetime(2020, 4, 1, 0, 0)},
+            {"completed_date": datetime(2020, 5, 1, 0, 0)},
+            {"completed_date": datetime(2020, 6, 30, 0, 0)},
+            {"completed_date": datetime(2020, 7, 1, 0, 0)},
+            {"completed_date": datetime(2020, 7, 1, 0, 0)},
+            {"completed_date": datetime(2020, 7, 1, 0, 0)},
+            {"completed_date": datetime(2020, 8, 1, 0, 0)},
+            {"completed_date": datetime(2020, 9, 30, 0, 0)},
+            {"completed_date": datetime(2020, 10, 1, 0, 0)},
+            {"completed_date": datetime(2020, 10, 1, 0, 0)},
+            {"completed_date": datetime(2020, 10, 1, 0, 0)},
+            {"completed_date": datetime(2020, 10, 1, 0, 0)},
+            {"completed_date": datetime(2020, 11, 1, 0, 0)},
+            {"completed_date": datetime(2020, 12, 31, 0, 0)}
+        ]
+
+        self.assertDictEqual(
+            BoardStats.monthly(1),
+            {
+                "name": "BOARD",
+                "records": {
+                    date(2020, 7, 31): 3,
+                    date(2020, 8, 31): 1,
+                    date(2020, 9, 30): 1,
+                    date(2020, 10, 31): 4,
+                    date(2020, 11, 30): 1,
+                    date(2020, 12, 31): 1,
+                }
+            }
+        )
 
 
 if __name__ == "__main__":
--- pythontest/trello/test_board.py
+++ pythontest/trello/test_board.py
@@ -1,13 +1,87 @@
+from datetime import datetime
 from unittest import main, TestCase
+from unittest.mock import Mock, patch
+
+from python.trello.board import Board
 
 
 class BoardTests(TestCase):
 
-    def test_init(self):
-        pass
+    _BASE_URL = "python.trello.board"
 
-    def test_completed(self):
-        pass
+    @patch(f"{_BASE_URL}.Request")
+    def test_init(self, mock_request):
+        board_id = "abc"
+        board_data = {"name": "BOARD"}
+
+        mock_request.return_value.get.return_value = board_data
+
+        board = Board(board_id)
+        self.assertEqual(board.board_id, board_id)
+        self.assertEqual(board.name, board_data["name"])
+
+    @staticmethod
+    def _request_data(object, id, return_object):
+        if object == "boards":
+            return [
+                {"id": 1, "name": "To Do"},
+                {"id": 2, "name": "In Progress"},
+                {"id": 3, "name": "Done"}
+            ]
+        elif object == "lists":
+            if id == 2:
+                return [
+                    {
+                        "name": "CARD-A",
+                        "due": "2020-12-31T12:30:45.000Z",
+                        "dueComplete": "2020-12-31"
+                    },
+                    {
+                        "name": "CARD-B",
+                        "due": "2020-12-31T12:30:45.000Z",
+                        "dueComplete": "2020-12-31"
+                    },
+                    {
+                        "name": "CARD-C",
+                        "due": "2020-12-31T12:30:45.000Z"
+                    }
+                ]
+            elif id == 3:
+                return [
+                    {
+                        "name": "CARD-D",
+                        "due": "2020-12-31T12:30:45.000Z",
+                        "dueComplete": "2020-12-31"
+                    },
+                    {
+                        "name": "CARD-E",
+                        "due": "2020-12-31T12:30:45.000Z",
+                        "dueComplete": "2020-12-31"
+                    },
+                    {
+                        "name": "CARD-F",
+                        "due": "2020-12-31T12:30:45.000Z"
+                    }
+                ]
+
+    @patch(f"{_BASE_URL}.Board.__init__")
+    def test_completed(self, mock_init):
+        mock_self = Mock(board_id=1)
+        mock_self._request.get.side_effect = self._request_data
+
+        self.assertListEqual(
+            Board.completed(mock_self),
+            [
+                {
+                    "title": "CARD-D",
+                    "completed_date": datetime(2020, 12, 31, 12, 30, 45)
+                },
+                {
+                    "title": "CARD-E",
+                    "completed_date": datetime(2020, 12, 31, 12, 30, 45)
+                }
+            ]
+        )
 
 
 if __name__ == "__main__":
